3bb7142089cb7b2e7787a59c27419ae1fd06e966
Deniss Jacenko <hi@deniss.co>
2022-07-14 19:03:46 -0400

    Create number generator

---
+++ main.js
@@ -0,0 +1,54 @@
+const button = document.getElementById("generate");
+const container = document.getElementById("container");
+
+// ------------------------------------------------------------------------------
+// Team member 1
+const generateNumbers = () => {
+	let numbers = [];
+
+	// Generate 6 random numbers from 1 to 53 and add them to the numbers array
+
+	return [1, 29, 14, 32, 47, 53]; // Return numbers when done
+};
+
+// ------------------------------------------------------------------------------
+// Team member 2
+const generateColorHex = () => {
+	let colorString = '';
+
+	// Generate a random hex color string here and asign it to colorString
+
+	return '#FEDCBA'; // Return colorString when done
+};
+
+// ---------------------------------------
+// Team member 3
+const getNumbersHtml = (numbers) => {
+	let numbersHtml = '';
+	
+	// Generate a string that looks like <div class="number">1</div><div class="number">29</div> ...
+	// Use generateColorHex() to generate background-color in inline CSS for each number div (style="background-color: #XXXXXX")
+
+	return '<div class="number">1</div><div class="number">29</div>'; // Return numbersHtml when done 
+};
+
+// ---------------------------------------
+// Team member 4
+const getIsAllEven = (numbers) => {
+	let isAllEven = false;
+
+	// Check if all lottery numbers in array are even and set isAllEven appropriately
+
+	 return isAllEven;
+};
+
+// ---------------------------------------
+// Finished generator
+button.addEventListener('click', () => {
+	console.log('Generator clicked!');
+	const numbers = generateNumbers();
+	const html = getNumbersHtml(numbers);
+	const numberTextColor = getIsAllEven() ? '#005500' : '#000000';
+	document.head.innerHTML += `<style>#container > div { color: ${numberTextColor} }</style>`;
+	container.innerHTML = html;
+});
