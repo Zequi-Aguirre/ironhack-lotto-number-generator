65b145c1aa30587055808b1f129c9a2510b631b2
Deniss Jacenko <hi@deniss.co>
2022-07-15 09:20:54 -0400

    Replace with spaces
    *For better viewing in Github

--- main.js
+++ main.js
@@ -5,43 +5,43 @@ const container = document.getElementById("container");
 // Team member 1
 // Generate 6 random numbers from 1 to 53 and add them to the numbers array
 const generateNumbers = () => {
-	let numbers = [];
+  let numbers = [];
 
-	for (let i = 0; i < 6; i++) {
-		let randomNumber = Math.floor(Math.random() * 53) + 1;
+  for (let i = 0; i < 6; i++) {
+    let randomNumber = Math.floor(Math.random() * 53) + 1;
 
-		if (numbers.includes(randomNumber)) {
-			console.log(randomNumber + " it was already in the array");
-		} else {
-			console.log(randomNumber);
-			numbers.push(randomNumber);
-			i++;
-		}
-	}
+    if (numbers.includes(randomNumber)) {
+      console.log(randomNumber + " it was already in the array");
+    } else {
+      console.log(randomNumber);
+      numbers.push(randomNumber);
+      i++;
+    }
+  }
 
-	numbers = numbers.sort(function (a, b) {
-		return a - b;
-	});
+  numbers = numbers.sort(function (a, b) {
+    return a - b;
+  });
 
-	return numbers;
+  return numbers;
 };
 
 // ------------------------------------------------------------------------------
 // Team member 2
 // Generate a random hex color string here and asign it to colorString
 const generateColorHex = () => {
-	let colorString = "";
+  let colorString = "";
 
-	colorString += "#";
-	let letterArray = ["6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-	for (let i = 0; i < 6; i++) {
-		let numberOfLetters = letterArray.length;
-		let randomIndex = Math.round(Math.random() * numberOfLetters);
-		let letterToAdd = letterArray[randomIndex];
-		colorString += letterToAdd;
-	}
+  colorString += "#";
+  let letterArray = ["6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+  for (let i = 0; i < 6; i++) {
+    let numberOfLetters = letterArray.length;
+    let randomIndex = Math.round(Math.random() * numberOfLetters);
+    let letterToAdd = letterArray[randomIndex];
+    colorString += letterToAdd;
+  }
 
-	return colorString;
+  return colorString;
 };
 
 // ------------------------------------------------------------------------------
@@ -49,41 +49,41 @@ const generateColorHex = () => {
 // Generate a string that looks like <div class="number">1</div><div class="number">29</div> ...
 // Use generateColorHex() to generate background-color in inline CSS for each number div (style="background-color: #XXXXXX")
 const getNumbersHtml = (numbers) => {
-	let numbersHtml = "";
+  let numbersHtml = "";
 
-	for (let i = 0; i < numbers.length; i++) {
-		numbersHtml += `<div style="background-color: ${generateColorHex()};" class="number">${
+  for (let i = 0; i < numbers.length; i++) {
+    numbersHtml += `<div style="background-color: ${generateColorHex()};" class="number">${
 			numbers[i]
 		}</div>`;
-	}
+  }
 
-	return numbersHtml;
+  return numbersHtml;
 };
 
 // ------------------------------------------------------------------------------
 // Team member 4
 // Check if all lottery numbers in array are even and set isAllEven appropriately
 const getIsAllEven = (numbers) => {
-	let isAllEven = true;
-	for (number of numbers) {
-		const isOdd = number % 2 !== 0;
-		console.log(number);
-		// if odd
-		if (isOdd) {
-			isAllEven = false;
-		}
-	}
+  let isAllEven = true;
+  for (number of numbers) {
+    const isOdd = number % 2 !== 0;
+    console.log(number);
+    // if odd
+    if (isOdd) {
+      isAllEven = false;
+    }
+  }
 
-	return isAllEven;
+  return isAllEven;
 };
 
 // ------------------------------------------------------------------------------
 // Finished generator
 button.addEventListener("click", () => {
-	console.log("Generator clicked!");
-	const numbers = generateNumbers();
-	const html = getNumbersHtml(numbers);
-	const numberTextColor = getIsAllEven(numbers) ? "#005500" : "#000000";
-	document.head.innerHTML += `<style>#container > div { color: ${numberTextColor} }</style>`;
-	container.innerHTML = html;
+  console.log("Generator clicked!");
+  const numbers = generateNumbers();
+  const html = getNumbersHtml(numbers);
+  const numberTextColor = getIsAllEven(numbers) ? "#005500" : "#000000";
+  document.head.innerHTML += `<style>#container > div { color: ${numberTextColor} }</style>`;
+  container.innerHTML = html;
 });
